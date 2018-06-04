a97f8d767fc2b8458197f22325a46bd39edb5e3e
jgabrielgv <jgmezvargas@gmail.com>
2018-06-03 21:57:09 -0600

    Validating posts data

--- ViewModels/ApplicationViewModel.cs
+++ ViewModels/ApplicationViewModel.cs
@@ -1,10 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace JobPortal.ViewModels
 {
     public class ApplicationViewModel
     {
+        [Required]
         public virtual int? CandidateId { get; set; }
+        [Required]
         public virtual int? JobId { get; set; }
     }
 }
